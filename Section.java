eb94c2cb0c553f25da7fc0f4bad76da2187de48e
Neel Shah <46041693+neelkshah@users.noreply.github.com>
2019-03-28 02:40:34 -0600

    Create Section.java

---
+++ Section.java
@@ -0,0 +1,11 @@
+public class Section {
+    public int courseCode;
+    int sectionID;
+    public int strength;
+
+    public Section(int courseCode, int sectionID, int strength){
+        this.courseCode = courseCode;
+        this.sectionID = sectionID;
+        this.strength = strength;
+    }
+}
