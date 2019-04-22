bf630fdb77eaff9a0b2fd7012ac0b2ea455e5af4
Neel Shah <46041693+neelkshah@users.noreply.github.com>
2019-04-22 11:41:31 +0530

    Add files via upload

---
+++ first-model/QueueMaker.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class QueueMaker{
+
+    public PriorityQueue<TimeTableObject>[] queue;
+
+    public QueueMaker(ArrayList<Student> students){
+        Comparator<TimeTableObject> comp = new Comparator<TimeTableObject>() {
+            @Override
+            public int compare(TimeTableObject t1, TimeTableObject t2) {
+                return t2.bidValue - t1.bidValue;
+            }
+        };
+        queue = new PriorityQueue[students.size()];
+        for(int i = 0; i < students.size(); i++){
+            queue[i] = new PriorityQueue<TimeTableObject>(comp);
+            for(int j = 0; j < students.get(i).timetable.size(); j++){
+                queue[i].add(students.get(i).timetable.get(j));
+            }
+        }
+    }
+
+    public void printQueues(){
+        for(PriorityQueue<TimeTableObject> pqueue: queue){
+            for(TimeTableObject t: pqueue){
+                System.out.println(t);
+                System.out.println();
+            }
+        }
+    }
+}
