5e6c0a9f982f9e797cbc34b3ee7c2e3742129483
Neel Shah <46041693+neelkshah@users.noreply.github.com>
2019-04-22 11:58:30 +0530

    Add files via upload

---
+++ third-model/Course.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Course {
+    public int courseCode;
+    public int coreBranch;
+    public ArrayList<Section> sectionList;
+    public int cstrength;
+
+    public Course(int courseCode, int coreBranch){
+        this.courseCode = courseCode;
+        this.coreBranch = coreBranch;
+        this.sectionList = new ArrayList<Section>();
+        this.cstrength = 0;
+    }
+
+    public void addSection(int sectionStrength){
+        Section s = new Section(this.courseCode, sectionList.size(), sectionStrength);
+        cstrength += sectionStrength;
+        sectionList.add(s);
+    }
+
+    public void removeSection(int sectionID){
+        cstrength -= sectionList.get(sectionID - 1).strength;
+        sectionList.remove(sectionID - 1);
+    }
+
+    public void printSectionList(){
+        for(int i = 0; i < sectionList.size(); i++){
+            System.out.println(sectionList.get(i));
+        }
+    }
+
+    public String toString(){
+        return "Course Code: " + courseCode + "\nBranch: " + coreBranch + "\nSections:\n" + sectionList.toString();
+    }
+}
---
+++ third-model/Student.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Student {
+    public int id;
+    public int branch;
+    public int balance;
+    public ArrayList<TimeTableObject> timetable;
+
+    public Student(int id, int branch, int points){
+        this.id = id;
+        this.branch = branch;
+        this.balance = points;
+        this.timetable = new ArrayList<TimeTableObject>();
+    }
+
+    public void addToTimetable(int course, int sectionNumber, int valueAssigned, boolean cdc){
+        TimeTableObject ttObj = new TimeTableObject(id, course, sectionNumber, valueAssigned, cdc);
+        timetable.add(ttObj);
+    }
+
+    public void printTimeTable(){
+        for(int i = 0; i < timetable.size(); i++){
+            System.out.println(timetable.get(i));
+        }
+    }
+
+    public String toString(){
+        return "ID number: " + id + "\nBranch: " + branch + "\nBalance: " + balance;
+    }
+}
